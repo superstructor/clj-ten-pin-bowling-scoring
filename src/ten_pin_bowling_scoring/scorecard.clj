9e52b8467e3901b4f0c797aa2acf6e2879516e8c
Isaac Johnston <isaac@xlm.sg>
2019-05-05 14:08:01 +0800

    Refactoring scorecard/score fn
    - rename frames to frames'
    - use destructuring for original frames in scorecard.

--- src/ten_pin_bowling_scoring/scorecard.clj
+++ src/ten_pin_bowling_scoring/scorecard.clj
@@ -15,50 +15,49 @@
   "Returns a new map representing a ten pin bowling scorecard with the rolls for
    a single frame 'added' and frames' scores recalculated. If the game is over a
    final score will also be provided."
-  [scorecard rolls]
-  (let [frames-rolls (conj (mapv :rolls (:frames scorecard))
-                           rolls)
-        frames (->> frames-rolls
-                    (map-indexed
-                      (fn [index frame-rolls]
-                        (let [frame-score (reduce + frame-rolls)]
-                          (cond
-                            (= 10 (first frame-rolls))
-                            (if (and (= 9 index)
-                                     (second frame-rolls)
-                                     (nth frame-rolls 2 nil))
-                              ;; Strike in last frame...
-                              {:rolls frame-rolls
-                               :score frame-score}
-                              (let [next-rolls (nth frames-rolls (inc index) nil)
-                                    nnext-rolls (nth frames-rolls (inc (inc index)) nil)
-                                    second-ball (first next-rolls)
-                                    third-ball (or (second next-rolls) (first nnext-rolls))]
-                                (if (and second-ball third-ball)
-                                  ;; Strike is 10 + next two rolls.
-                                  {:rolls frame-rolls
-                                   :score (+ 10 second-ball third-ball)}
-                                  ;; Strike is not complete yet, so does not have a score.
-                                  {:rolls frame-rolls})))
+  [{:keys [frames] :as scorecard} rolls]
+  (let [frames-rolls (conj (mapv :rolls frames) rolls)
+        frames' (->> frames-rolls
+                     (map-indexed
+                       (fn [index frame-rolls]
+                         (let [frame-score (reduce + frame-rolls)]
+                           (cond
+                             (= 10 (first frame-rolls))
+                             (if (and (= 9 index)
+                                      (second frame-rolls)
+                                      (nth frame-rolls 2 nil))
+                               ;; Strike in last frame...
+                               {:rolls frame-rolls
+                                :score frame-score}
+                               (let [next-rolls (nth frames-rolls (inc index) nil)
+                                     nnext-rolls (nth frames-rolls (inc (inc index)) nil)
+                                     second-ball (first next-rolls)
+                                     third-ball (or (second next-rolls) (first nnext-rolls))]
+                                 (if (and second-ball third-ball)
+                                   ;; Strike is 10 + next two rolls.
+                                   {:rolls frame-rolls
+                                    :score (+ 10 second-ball third-ball)}
+                                   ;; Strike is not complete yet, so does not have a score.
+                                   {:rolls frame-rolls})))
 
-                            (= 10 frame-score)
-                            (if (and (= 9 index)
-                                     (second frame-rolls)
-                                     (nth frame-rolls 2 nil))
-                              ;; Spare in last frame...)
-                              {:rolls frame-rolls
-                               :score frame-score}
-                              (if-let [next-rolls (nth frames-rolls (inc index) nil)]
-                                ;; Spare is 10 + first roll of next frame.
-                                {:rolls frame-rolls
-                                 :score (+ 10 (first next-rolls))}
-                                ;; Spare is not complete yet, so does not have a score.
-                                {:rolls frame-rolls}))
+                             (= 10 frame-score)
+                             (if (and (= 9 index)
+                                      (second frame-rolls)
+                                      (nth frame-rolls 2 nil))
+                               ;; Spare in last frame...)
+                               {:rolls frame-rolls
+                                :score frame-score}
+                               (if-let [next-rolls (nth frames-rolls (inc index) nil)]
+                                 ;; Spare is 10 + first roll of next frame.
+                                 {:rolls frame-rolls
+                                  :score (+ 10 (first next-rolls))}
+                                 ;; Spare is not complete yet, so does not have a score.
+                                 {:rolls frame-rolls}))
 
-                            :open-frame
-                            {:rolls frame-rolls
-                             :score frame-score}))))
-                    (vec))]
-    (cond-> {:frames frames}
-            (= 10 (count frames))
-            (assoc :score (total frames)))))
+                             :open-frame
+                             {:rolls frame-rolls
+                              :score frame-score}))))
+                     (vec))]
+    (cond-> {:frames frames'}
+            (= 10 (count frames'))
+            (assoc :score (total frames')))))
