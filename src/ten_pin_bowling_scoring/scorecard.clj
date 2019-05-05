970fae86baa9cacc16a7e2898c0e70f23bf806bf
Isaac Johnston <isaac@xlm.sg>
2019-05-05 10:43:25 +0800

    Fix failing test for create an empty scorecard
    Add scorecard ns and scorecard/create fn.

---
+++ src/ten_pin_bowling_scoring/scorecard.clj
@@ -0,0 +1,8 @@
+(ns ten-pin-bowling-scoring.scorecard)
+
+(defn create
+  "Returns a new map representing a ten pin bowling scorecard; i.e. the initial
+   state of the game."
+  []
+  {:frames []})
+
