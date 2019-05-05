c48c3bcc5268018945236d3a16ec1b38198fa354
Isaac Johnston <isaac@xlm.sg>
2019-05-05 10:33:07 +0800

    Add test runner

---
+++ test/ten_pin_bowling_scoring/placeholder_test.clj
@@ -0,0 +1,7 @@
+(ns ten-pin-bowling-scoring.placeholder-test
+  (:require
+    [clojure.test :refer [deftest is testing]]))
+
+(deftest placeholder-test
+  (testing "the cognitect test runner works"
+    (is true)))
