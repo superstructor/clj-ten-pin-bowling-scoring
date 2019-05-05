2e2758d9e184caa6b70663a2d47fddba2872d19d
Isaac Johnston <isaac@xlm.sg>
2019-05-05 10:58:40 +0800

    Fix failing test for scoring of a gutter game
    Add scorecard/score fn.

--- src/ten_pin_bowling_scoring/scorecard.clj
+++ src/ten_pin_bowling_scoring/scorecard.clj
@@ -6,3 +6,15 @@
   []
   {:frames []})
 
+(defn score
+  "Returns a new map representing a ten pin bowling scorecard with the rolls for
+   a single frame 'added' and frames' scores recalculated. If the game is over a
+   final score will also be provided."
+  [scorecard rolls]
+  (let [frame-score (reduce + rolls)
+        frame {:rolls rolls
+               :score frame-score}
+        frames (conj (:frames scorecard) frame)
+        score (reduce + (map :score frames))]
+    {:frames frames
+     :score  score}))
